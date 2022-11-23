f2c56722793ee726985173e9e3d37bafadf954bd
“backSeungWook” <tmxk3856@gmail.com>
2022-11-23 22:39:16 +0900

    dashboard START

---
+++ src/Menu/Menu.js
@@ -0,0 +1,90 @@
+
+import Parking from '../View/ParkingSms/Parking'
+
+
+// 주정차문자알림서비스 메뉴
+const parking = [
+  {
+    key:1,
+    value:'서비스 발송결과'
+  },
+  {
+    key:2,
+    value:'서비스 발송결과 통계'
+  },
+  {
+    key:3,
+    value:'전체 통계'
+  },
+  {
+    key:4,
+    value:'전체 장비 데이터'
+  },
+]
+
+// 서비스신청 메뉴
+const member = [
+  {
+    key:1,
+    value:'홈페이지신청자',
+    children:Parking
+  },
+  {
+    key:2,
+    value:'홈페이지신청자통계',
+    children:Parking
+  },
+  {
+    key:3,
+    value:'블랙리스트',
+    children:Parking
+  },
+  {
+    key:4,
+    value:'블랙리스트통계',
+    children:Parking
+  },
+  {
+    key:5,
+    value:'신청자엑셀업로드',
+    children:Parking
+  },
+  {
+    key:6,
+    value:'신청자메시지발송내역',
+    children:Parking
+  },
+  {
+    key:7,
+    value:'신청자메시지발송통계',
+    children:Parking
+  },
+]
+
+const module =[{key:1,value:'CCTV연계 모듈',children:Parking}]
+
+const mainCategory = [
+  {
+    key:1,
+    value:'주정차 문자알림서비스',
+    category:parking,  
+  },
+  {
+    key:2,
+    value:'신청서비스',
+    category:member,
+    
+  },
+  {
+    key:3,
+    value:'연계 모듈',
+    category:module,
+  },
+]
+
+
+
+export {
+  mainCategory,
+
+}
---
+++ src/View/Main/Main.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react'
+import './CSS/main.scss'
+import Parking from '../ParkingSms/Parking'
+import {mainCategory} from '../../Menu/Menu'
+import styled from "styled-components";
+// import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+
+// STYLED 
+const HeaderDiv = styled.div`
+  display:flex;
+  color:#fff;
+`
+
+
+
+
+export default function Main() {
+
+  const items2 = mainCategory.map((data, index) => {
+  
+    return {
+      key: data.key,
+      // icon: React.createElement(icon),
+      label: `${data.value}`,
+      onClick:(e)=>{
+        setContent(data.category.filter(subData => e.key === subData.value)[0].children)
+        // console.log(data.category.filter(subData => e.key === subData.value)[0].children)
+  
+        // data.category.map((subData, j) => {
+        //   console.log(subData.children)
+        // })
+      },
+      children: data.category.map((subData, j) => {
+        
+        return {
+          key: `${subData.value}`,
+          label: `${subData.value}`,        
+        }
+        
+      }),
+    };
+  });
+
+  const [content, setContent] = useState('')
+  
+
+  const menuEvent = (e)=>{
+    // setContent(Parking)
+    console.log('ss')
+  }
+
+  return (
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: '64px',
+          bottom: 0,
+        }}
+      >
+        
+        <Menu
+          mode="inline"
+          theme={'dark'}
+          // defaultSelectedKeys={['1']}
+          // defaultOpenKeys={['sub1']}
+          style={{
+            height: '100%',
+            borderRight: 0,
+            color:'#fff',
+            fontWeight:'600'
+          }}
+          items={items2}
+          onClick={(e)=>menuEvent(e)}
+        />
+      </Sider>
+
+      <Layout
+        className="site-layout"
+        style={{
+          // marginLeft: 200,
+        }}
+      >
+        <Header
+          className="site-layout-background"
+          style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}
+        >
+          <HeaderDiv>
+            
+            <div style={{width:'150px'}}>
+              로그인
+            </div>    
+            <div style={{paddingLeft:'20px',fontSize:'20px',fontWeight:'600'}}>
+              OOO 사전단속알림시스템
+            </div>    
+            <div style={{
+              position: 'absolute',
+              right: '10px'
+            }}>
+              로그아웃
+            </div>
+          </HeaderDiv>
+          
+        </Header>
+        {/* 본문 */}
+        <Content
+          style={{
+            // margin: '24px 16px 0',
+            margin: '0px 0px 0px 200px',
+            paddingLeft: '15px',
+            paddingTop:'10px',
+            overflow: 'initial',
+          }}
+        >
+          
+          <div
+            className="site-layout-background"
+            style={{
+              // padding: 24, 
+              // textAlign: 'center',
+            }}
+          >
+            {content}
+          </div>
+        </Content>
+
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+
+    </Layout>
+  )
+}
